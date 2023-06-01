d61296dd97f05354b22336ededa2d95e9da13a1d
Chunmyong Park <chunmyong.park@kaist.ac.kr>
2023-06-02 01:41:49 +0900

    Simulate triton server

--- Part_1-Simple_Serving_System/6-Scheduler_Stub/main.py
+++ Part_1-Simple_Serving_System/6-Scheduler_Stub/main.py
@@ -10,12 +10,68 @@ class InferData(BaseModel):
     outputs: Any
 
 
+@app.get("/v2/models/{model}")
+async def get_metadata(model: str):
+    # TODO: obtained from model registry in the future
+    dummy = {
+        "name": model,
+        "inputs": [
+            {"name": "input__0", "datatype": "FP32", "shape": [3, 224, 224]}
+        ],
+        "outputs": [
+            {"name": "output__0", "datatype": "FP32", "shape": [1000]}
+        ],
+    }
+    return dummy
+
+
+@app.get("/v2/models/{model}/config")
+async def get_config(model: str):
+    # TODO: obtained from model registry in the future
+    dummy = {
+        "name": model,
+    }
+    return dummy
+
+
+@app.get("/v2/models/{model}/stats")
+async def get_stats(model: str):
+    # TODO: obtained from model registry in the future
+    dummy = {
+        "name": model,
+    }
+    return dummy
+
+
+@app.get("/v2")
+async def get_api_metadata():
+    # NOTE: simulate triton server
+    dummy = {
+        "name": "triton",
+        "version": "2.33.0",
+        "extensions": [
+            "classification",
+            "sequence",
+            "model_repository",
+            "model_repository(unload_dependents)",
+            "schedule_policy",
+            "model_configuration",
+            "system_shared_memory",
+            "cuda_shared_memory",
+            "binary_tensor_data",
+            "parameters",
+            "statistics",
+            "trace",
+            "logging",
+        ],
+    }
+    return dummy
+
+
 @app.post("/v2/models/{model}/infer")
 async def infer(model: str, infer_data: InferData):
     print(f"Received request for model {model} with data: {infer_data}")
-
-    # 여기서 실제로 모델의 추론을 수행할 수 있습니다.
-    # 여기에서는 간단하게 요청 데이터를 그대로 반환하도록 하겠습니다.
     return infer_data
+
 
 # Run this server with: uvicorn main:app --reload
